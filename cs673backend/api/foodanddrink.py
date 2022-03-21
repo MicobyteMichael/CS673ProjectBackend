6085706ffb2fdef770f01d0399779f87ee6e6162
Michael Tuma <mike.tuma@hotmail.com>
2022-03-21 15:45:22 -0400

    Added support for tracking meals

--- cs673backend/api/foodanddrink.py
+++ cs673backend/api/foodanddrink.py
@@ -9,6 +9,13 @@ def start(flaskapp, db, api):
 		day     = db.Column(db.Integer, primary_key = True)
 		glasses = db.Column(db.Integer, nullable = False)
 	
+	class Meals(db.Model):
+		userid   = db.Column(db.Integer, primary_key = True)
+		year     = db.Column(db.Integer, primary_key = True)
+		day      = db.Column(db.Integer, primary_key = True)
+		meal     = db.Column(db.String,  primary_key = True)
+		calories = db.Column(db.Integer, nullable = False)
+	
 	class WaterGlasses(Resource):
 		def __init__(self):
 			self.parser = RequestParser()
@@ -48,4 +55,39 @@ def start(flaskapp, db, api):
 			else:
 				return { "error": "Not signed in" }
 	
+	class MealsEaten(Resource):
+		def __init__(self):
+			self.parser = RequestParser()
+			self.parser.add_argument("year", type = int, required = True)
+			self.parser.add_argument("day", type = int, required = True)
+			
+			self.parser2 = RequestParser()
+			self.parser.add_argument("mealname", type = str, required = True)
+			self.parser2.add_argument("calories", type = int, required = True)
+		
+		def post(self):
+			args = self.parser.parse_args(strict = True)
+			
+			if "userid" in session:
+				meals = (Meals.query.filter_by(userid = session["userid"], year = args["year"], day = args["day"]).all()) or []
+				return [ { "meal": meal.meal, "calories": meal.calories } for meal in meals ]
+			else:
+				return { "error": "Not signed in" }
+		
+		def put(self):
+			args = self.parser.parse_args() | self.parser2.parse_args()
+			
+			if "userid" in session:
+				existint_meal = Meals.query.filter_by(userid = session["userid"], year = args["year"], day = args["day"], meal = args["meal"]).first()
+				if existint_meal is not None:
+					return { "error": "A meal with that name is already added" }
+				
+				db.session.add(Meals(userid = session["userid"], year = args["year"], day = args["day"], meal = args["meal"], calories = args["calories"]))
+				db.session.commit()
+				
+				return { "added": True }
+			else:
+				return { "error": "Not signed in" }
+	
 	api.add_resource(WaterGlasses, "/waterintake")
+	api.add_resource(MealsEaten,   "/mealintake")
