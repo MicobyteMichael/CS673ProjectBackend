a8feb5d475d65e4c4262e85b18ab035569cbc192
Michael Tuma <mike.tuma@hotmail.com>
2022-03-22 12:22:38 -0400

    Added support for enabling and disabling certain goals

--- cs673backend/api/goalssystem.py
+++ cs673backend/api/goalssystem.py
@@ -9,6 +9,14 @@ def start(flaskapp, db, api):
 		type   = db.Column(db.String,  nullable    = False)
 		comp   = db.Column(db.String,  nullable    = False)
 		thresh = db.Column(db.Integer, nullable    = False)
+		active = db.Column(db.Boolean, nullable    = False)
+	
+	class GoalAchievements(db.Model):
+		userid   = db.Column(db.Integer, primary_key = True )
+		year     = db.Column(db.Integer, primary_key = True )
+		day      = db.Column(db.Integer, primary_key = True )
+		name     = db.Column(db.String,  primary_key = True )
+		achieved = db.Column(db.Boolean, nullable    = False)
 	
 	class GoalTracking(Resource):
 		def __init__(self):
@@ -19,6 +27,9 @@ def start(flaskapp, db, api):
 			self.parser2.add_argument("type",   type = str, required = True)
 			self.parser2.add_argument("comp",   type = str, required = True)
 			self.parser2.add_argument("thresh", type = int, required = True)
+			
+			self.parser3 = RequestParser()
+			self.parser3.add_argument("active", type = bool, required = True)
 		
 		def get(self):
 			if "userid" in session:
@@ -48,10 +59,25 @@ def start(flaskapp, db, api):
 				if existing_goal is not None:
 					return { "error": "A goal with that name is already added" }
 				
-				db.session.add(Goals(userid = session["userid"], name = args["name"], type = args["type"], comp = args["comp"], thresh = args["thresh"]))
+				db.session.add(Goals(userid = session["userid"], name = args["name"], type = args["type"], comp = args["comp"], thresh = args["thresh"], active = True))
 				db.session.commit()
 				
 				return { "added": True }
+			else:
+				return { "error": "Not signed in" }
+		
+		def patch(self):
+			args = self.parser.parse_args() | self.parser3.parse_args()
+			
+			if "userid" in session:
+				goal = Goals.query.filter_by(userid = session["userid"], name = args["name"]).first()
+				if goal is None:
+					return { "error": "No goal exists by that name" }
+				
+				goal.active = args["active"]
+				db.session.commit()
+				
+				return { "updated": True }
 			else:
 				return { "error": "Not signed in" }
 	
